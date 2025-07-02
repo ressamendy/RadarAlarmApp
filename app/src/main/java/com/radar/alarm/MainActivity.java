4ac530f3bf836ae4596516fcc40c83d35afde1c8
Ender Vural <endervural@gmail.com>
2025-07-02 17:04:49 +0300

    İlk yükleme

---
+++ app/src/main/java/com/radar/alarm/MainActivity.java
@@ -0,0 +1,19 @@
+
+package com.radar.alarm;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://192.168.1.26");
+        setContentView(webView);
+    }
+}
